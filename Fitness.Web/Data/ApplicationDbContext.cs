66a150ccc0f03711fdd16c6e7f5cac8915bc92d7
nikovlyubomir <nikovlyubomir@gmail.com>
2018-05-24 12:46:24 +0200

    initial commit

---
+++ Fitness.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Fitness.Web.Data.Models;
+using Fitness.Web.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Fitness.Web.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {}
+
+        public DbSet<Workout> Workout { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Fitness.Web/Data/Models/Workout.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fitness.Web.Data.Models
+{
+    public class Workout
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public double CaloriesBurned { get; set; }
+        public int DurationMinutes { get; set; }
+    }
+}

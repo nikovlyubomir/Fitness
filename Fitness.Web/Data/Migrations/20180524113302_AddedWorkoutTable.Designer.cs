0199c65160346a04e5d2b73a9edef50ba3f40308
nikovlyubomir <nikovlyubomir@gmail.com>
2018-05-24 13:41:13 +0200

    added migration for new Table

--- Fitness.Web/Data/Migrations/20180524113302_AddedWorkoutTable.Designer.cs
+++ Fitness.Web/Data/Migrations/20180524113302_AddedWorkoutTable.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace Fitness.Web.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180524113302_AddedWorkoutTable")]
+    partial class AddedWorkoutTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
